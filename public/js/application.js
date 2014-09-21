d1ca85ffc3b9625ae2167f71b5ee737fffaf46a2
Jenny Yang <hyeayouy@andrew.cmu.edu>
2014-09-21 02:51:06 -0400

    added more buttons

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,79 @@
+// index #buttons
+$(function avocadoInsta() {
+  $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: "https://api.instagram.com/v1/tags/avocado/media/recent?client_id=f084959fc2c24ca38cd1083389de62a0",
+    success: function(data) {
+      // placing the images on the page
+      for (var i = 0; i < 20; i++) {
+        $("#images").append("<img src=" + data.data[i].link + "media\/?size=t>");
+      };
+    }
+  });
+});
+
+$(function guacInsta() {
+  $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: "https://api.instagram.com/v1/tags/guacamole/media/recent?client_id=f084959fc2c24ca38cd1083389de62a0",
+    success: function(data) {
+      // placing the images on the page
+      for (var i = 0; i < 20; i++) {
+        $("#images").append("<img src=" + data.data[i].link + "media\/?size=t>");
+      };
+    }
+  });
+});
+
+$(function eggoInsta() {
+  $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: "https://api.instagram.com/v1/tags/eggocado/media/recent?client_id=f084959fc2c24ca38cd1083389de62a0",
+    success: function(data) {
+      // placing the images on the page
+      for (var i = 0; i < 20; i++) {
+        $("#images").append("<img src=" + data.data[i].link + "media\/?size=t>");
+      };
+    }
+  });
+});
+
+$(function pastaInsta() {
+  $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: "https://api.instagram.com/v1/tags/avocadopasta/media/recent?client_id=f084959fc2c24ca38cd1083389de62a0",
+    success: function(data) {
+      // placing the images on the page
+      for (var i = 0; i < 20; i++) {
+        $("#images").append("<img src=" + data.data[i].link + "media\/?size=t>");
+      };
+    }
+  });
+});
+
+$(function rollInsta() {
+  $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: "https://api.instagram.com/v1/tags/avocadoroll/media/recent?client_id=f084959fc2c24ca38cd1083389de62a0",
+    success: function(data) {
+      // placing the images on the page
+      for (var i = 0; i < 20; i++) {
+        $("#images").append("<img src=" + data.data[i].link + "media\/?size=t>");
+      };
+    }
+  });
+});
+
 // Avocado FAQ
 $(function() {    // do once original document loaded and ready
 	$.getJSON("avocadoFAQ.json", function(response, diditwork) {
