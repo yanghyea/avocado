03b736194d086a0820eed8d74a77a82f98d5cc20
Jenny Yang <hyeayouy@andrew.cmu.edu>
2014-09-20 22:39:31 -0400

    Added facts page

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,4 @@
+// Avocado FAQ
 $(function() {    // do once original document loaded and ready
 	$.getJSON("avocadoFAQ.json", function(response, diditwork) {
       console.log(diditwork);
@@ -10,5 +11,21 @@ $(function() {    // do once original document loaded and ready
               }
       displayT += "<\/ul>";
   $("#faqArea").html(displayT);
+  });  // getJSON
+}); // onReady
+
+// Avocado Fun Facts
+$(function() {    // do once original document loaded and ready
+  $.getJSON("avocadoFacts.json", function(response, diditwork) {
+      console.log(diditwork);
+      var displayT = "";
+      for (var i = 0; i < response.facts.length; i ++) {
+              var entry = response.facts[i];
+              displayT += "<li>"
+                + entry.title + " " 
+                + entry.fact + "<\/li>";
+              }
+      displayT += "<\/ul>";
+  $("#factsArea").html(displayT);
   });  // getJSON
 }); // onReady
