6bc9decdd2dd76870fd4b5857a4bd50ad62145f1
Jenny Yang <hyeayouy@andrew.cmu.edu>
2014-09-22 17:32:49 -0400

    Added comments to memory js file

--- public/js/memory.js
+++ public/js/memory.js
@@ -2,16 +2,18 @@ var boxopened = "";
 var imgopened = "";
 var found =  0;
 
+// function that randomly chooses a number within the size of the array
 function randomize(first, last){
     return Math.floor(Math.random() * (last - first + 1) + first);
 }
 
 function shuffle() {
     var children = $("#grid").children();
-    var child = $("#grid div:first-child");
+    var child = $("#grid div:first-child"); 
 
     var array_img = new Array();
 
+    // go through every cell of the grid and give the 'img' id to the cells
     for (i=0; i<children.length; i++) {
         array_img[i] = $("#"+child.attr("id")+" img").attr("src");
         child = child.next();
@@ -19,6 +21,7 @@ function shuffle() {
 
     var child = $("#grid div:first-child");
 
+    // attribute a random index to each cell of the grid which corresponds to the image id
     for (z=0; z<children.length; z++) {
         randIndex = randomize(0, array_img.length - 1);
 
@@ -30,6 +33,8 @@ function shuffle() {
     }
 }
 
+// resets all global variables, shuffles the game board and 
+// hides images 
 function resetGame() {
     shuffle();
 	var solved_div = document.getElementById('solved_game');
@@ -43,6 +48,8 @@ function resetGame() {
 }
 
 $(document).ready(function() {
+    // hides all images in the beginnign of the game and
+    // opens cards on click 
     $("img").hide();
     $("#grid div").click(openCard);
 
@@ -51,7 +58,8 @@ $(document).ready(function() {
     function openCard() {
 
         id = $(this).attr("id");
-
+        // all images are hidden initially, 
+        // if they want to be opened, fade in the image
         if ($("#"+id+" img").is(":hidden")) {
             $("#grid div").unbind("click", openCard);
 
@@ -66,7 +74,7 @@ $(document).ready(function() {
             } else {
                 currentopened = $("#"+id+" img").attr("src");
                 if (imgopened != currentopened) {
-                    // close again
+                    // close opened cards if they don't match (fade out)
                     setTimeout(function() {
                         $("#"+id+" img").fadeOut('fast');
                         $("#"+boxopened+" img").fadeOut('fast');
@@ -74,9 +82,10 @@ $(document).ready(function() {
                         imgopened = "";
                     }, 400);
                 } else {
-                    // found
+                    // keep cards opened if they match (take out opacity)
                     $("#"+id+" img").addClass("opacity");
                     $("#"+boxopened+" img").addClass("opacity");
+                    // increase the found counter by 1
                     found++;
                     boxopened = "";
                     imgopened = "";
@@ -86,7 +95,8 @@ $(document).ready(function() {
                     $("#grid div").bind("click", openCard)
                 }, 400);
             }
-			
+			// if all cards are found, display the congratulations message
+            // by making the solved div visible
             if (found == 8) {
 				var solved_div = document.getElementById('solved_game');
 				solved_div.style.visibility = 'visible';
